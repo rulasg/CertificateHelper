b30005a0defee6b3acee4bc176a081bbd32c6c52
rulasg <rulasg@github.com>
2024-01-31 09:27:30 +0100

    get-userinfo

--- CertificateHelperTest/public/getUserName.test.ps1
+++ CertificateHelperTest/public/getUserName.test.ps1
@@ -12,3 +12,20 @@ function CertificateHelperTest_GetUserName_Success{
 
     Assert-AreEqual -Expected $name -Presented $result
 }
+
+function CertificateHelperTest_GetUserInfo_Success{
+
+    $handle = "josess"
+    $name = "Joe Smith"
+
+    $mockFile = $PSScriptRoot | Join-Path -ChildPath 'testdata' -AdditionalChildPath 'getuserSuccess.json'
+    Set-InvokeCommandAlias -Alias "gh api users/$handle" -Tag 'CertificateHelperMock' -Command "Get-Content -Path $(($mockFile | Get-Item).FullName)"
+
+    $result = Get-UserInfo -UserHandle $handle
+
+    Assert-AreEqual -Expected $name -Presented $result.Name
+    Assert-AreEqual -Expected $handle -Presented $result.Handle
+    Assert-AreEqual -Expected "Contoso" -Presented $result.Company
+    Assert-AreEqual -Expected "josess@github.com" -Presented $result.email
+
+}
--- public/getStudentname.ps1
+++ public/getStudentname.ps1
@@ -22,3 +22,33 @@ function Get-UserName{
         }
     }
 } Export-ModuleMember -Function Get-UserName
+
+function Get-UserInfo{
+    [CmdletBinding()]
+    param (
+        [Parameter(Position=0,ValueFromPipeline)][string]$UserHandle
+    )
+
+    process{
+        
+        $parameters = @{
+            userhandle = $UserHandle
+        }
+
+        $result = Invoke-MyCommandJson -Command GetUser -Parameters $parameters
+
+        if($result){
+            $ret = [PSCustomObject]@{
+                Name = $result.name
+                Handle = $result.login
+                Company = $result.company
+                email = $result.email
+            }
+
+        } else {
+            $ret = $null
+        }
+
+        return $ret
+    }
+} Export-ModuleMember -Function Get-UserInfo
